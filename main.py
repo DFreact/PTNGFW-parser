f503d54310b420158e7981679027211a9094a2e7
Danila <36110829+DFreact@users.noreply.github.com>
2025-07-13 22:14:23 +0300

    Unlock export function main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     html_path = "save_html/"
     config_path = "config_secure.json"
 
-    #dump_all_json(config_path=config_path, save_path=json_path)
+    dump_all_json(config_path=config_path, save_path=json_path)
     export_all_html_and_csv(json_path=json_path, html_path=html_path, use_color=True)
